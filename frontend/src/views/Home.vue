8dad7e45a72a8e84d87a3d26c12e214b05a213a8
Sebastian Klein <zugende@gmail.com>
2020-03-21 00:46:24 +0100

    added vue boilerplate to the project

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+        <h1>Welcome to Coronakarma</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    components: {}
+};
+</script>
