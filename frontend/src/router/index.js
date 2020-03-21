78e0460f6dfead4b191a263eb4beddcce09ab9f8
Sebastian Klein <zugende@gmail.com>
2020-03-21 18:44:52 +0100

    added /thank-you page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import KkDashboard from "../views/KkDashboard.vue";
 import KkHome from "../views/KkHome.vue";
 import KkAssignmentOverview from "../views/KkAssignmentOverview.vue";
+import KkThankYou from "../views/KkThankYou.vue";
 
 Vue.use(VueRouter);
 
@@ -17,11 +18,16 @@ const routes = [
 		name: "Dashboard",
 		component: KkDashboard
 	},
-    {
-        path: "/auftraege",
-        name: "AssignmentOverview",
-        component: KkAssignmentOverview
-    },
+	{
+		path: "/auftraege",
+		name: "AssignmentOverview",
+		component: KkAssignmentOverview
+	},
+	{
+		path: "/thank-you",
+		name: "ThankYou",
+		component: KkThankYou
+	}
 ];
 
 const router = new VueRouter({
