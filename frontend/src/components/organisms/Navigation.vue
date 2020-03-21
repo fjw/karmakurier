b985fa8f2d35405df0cf0161f466afd0123bca10
Sebastian Klein <zugende@gmail.com>
2020-03-21 16:39:50 +0100

    moved Buefy config in external Branding.scss file and added Quicksand font

--- frontend/src/components/organisms/Navigation.vue
+++ frontend/src/components/organisms/Navigation.vue
@@ -9,15 +9,17 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
+@import "@/Components/Branding.scss";
+
 .navigation-wrapper {
     padding: 12px 0 12px 0;
-    height: 24px;
+    height: 48px;
     width: 100%;
     display: flex;
     justify-content: center;
     align-content: center;
-    box-shadow: 0px 4px 4px rgba(black, 0.02);
-    border-bottom: solid 1px rgba(black, 0.08);
+    box-shadow: 0px 4px 4px rgba(black, 0.04);
+    background-color: $primary;
     img {
         height: 100%;
         width: auto;
--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="headline">
-    <h1>Hier kommt das Dashboard hin</h1>
-  </div>
+    <div class="headline">
+        <h1>Hier kommt das Dashboard hin</h1>
+        <b-button type="is-primary">Das ist ein cooler Button</b-button>
+    </div>
 </template>
 
 <script>
 export default {
-  components: {}
+    components: {}
 };
 </script>
