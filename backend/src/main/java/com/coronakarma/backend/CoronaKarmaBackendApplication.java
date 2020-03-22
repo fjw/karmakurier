715388c17dffb495a3770a7663f9cf68db7310bc
Sebastian Blam <sebastian.blam@senacor.com>
2020-03-22 17:32:07 +0100

    cleanup main

--- backend/src/main/java/com/coronakarma/backend/CoronaKarmaBackendApplication.java
+++ backend/src/main/java/com/coronakarma/backend/CoronaKarmaBackendApplication.java
@@ -14,15 +14,6 @@ public class CoronaKarmaBackendApplication {
 
 	public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(CoronaKarmaBackendApplication.class, args);
-
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-                System.out.println(beanName);
-        }   
-
 	}
 
 }
