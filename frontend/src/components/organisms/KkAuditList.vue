5614dbb9a7952b61b9e180cb0f0b2ab882872ecd
Mathias Wellner <mathias@mwellner.de>
2020-03-22 15:49:17 +0100

    rename "Aufträge" to "Missionen"

--- frontend/src/components/organisms/KkAuditList.vue
+++ frontend/src/components/organisms/KkAuditList.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="wrapper">
-        <div class="overview-wrapper">
-            <div class="title">
-                <span>{{assignments.length}}</span> Belege müssen geprüft werden.
-            </div>
-        </div>
-
-        <div class="plzfilterinput">
-            <b-field label="PLZ">
-                <b-input v-model="zipCode"></b-input>
-            </b-field>
-
-            <p v-if="zipCode === ''">Bitte geben Sie Ihre Postleitzahl an.</p>
-
-            <p
-                v-if="zipCode !== '' && assignments.length === 0"
-            >Leider wurden keine Aufträge gefunden. Bitte suchen Sie auch nach Postleitzahlen im Umkreis.</p>
-        </div>
-
-        <KkTaskItem
-            class
-            v-for="assignment in assignments"
-            :key="assignment.id"
-            :assignment="assignment"
-        ></KkTaskItem>
-
-        <b-loading :active="loading"></b-loading>
+  <div class="wrapper">
+    <div class="overview-wrapper">
+      <div class="title">
+        <span>{{assignments.length}}</span> Belege müssen geprüft werden.
+      </div>
     </div>
+
+    <div class="plzfilterinput">
+      <b-field label="PLZ">
+        <b-input v-model="zipCode"></b-input>
+      </b-field>
+
+      <p v-if="zipCode === ''">Bitte geben Sie Ihre Postleitzahl an.</p>
+
+      <p
+        v-if="zipCode !== '' && assignments.length === 0"
+      >Leider wurden keine Missionen gefunden. Bitte suchen Sie auch nach Postleitzahlen im Umkreis.</p>
+    </div>
+
+    <KkTaskItem
+      class
+      v-for="assignment in assignments"
+      :key="assignment.id"
+      :assignment="assignment"
+    ></KkTaskItem>
+
+    <b-loading :active="loading"></b-loading>
+  </div>
 </template>
 
 <script>
@@ -36,72 +36,72 @@ import debounce from "lodash.debounce";
 import parseISO from "date-fns/parseISO";
 
 export default {
-    components: { KkTaskItem },
-    data() {
-        return {
-            // Zip code filter.
-            zipCode: "",
+  components: { KkTaskItem },
+  data() {
+    return {
+      // Zip code filter.
+      zipCode: "",
 
-            // Assignments for zip code filter.
-            assignments: [],
+      // Assignments for zip code filter.
+      assignments: [],
 
-            // Total number of assignments.
-            assignmentCount: 12,
+      // Total number of assignments.
+      assignmentCount: 12,
 
-            // API client.
-            client: null,
+      // API client.
+      client: null,
 
-            // Loading state.
-            loading: false
-        };
-    },
+      // Loading state.
+      loading: false
+    };
+  },
 
-    watch: {
-        /**
-         * Watch user input with debounce to not call API too often.
-         */
-        zipCode: debounce(function(value) {
-            if (value !== "") {
-                this.fetchAssignmentsForZip();
-            } else {
-                this.assignments = [];
-            }
-        }, 750)
-    },
+  watch: {
+    /**
+     * Watch user input with debounce to not call API too often.
+     */
+    zipCode: debounce(function(value) {
+      if (value !== "") {
+        this.fetchAssignmentsForZip();
+      } else {
+        this.assignments = [];
+      }
+    }, 750)
+  },
 
-    methods: {
-        /**
-         * Fetch assignments for currently entered ZIP code.
-         */
-        fetchAssignmentsForZip() {
-            // Set loading state.
-            this.loading = true;
+  methods: {
+    /**
+     * Fetch assignments for currently entered ZIP code.
+     */
+    fetchAssignmentsForZip() {
+      // Set loading state.
+      this.loading = true;
 
-            // Make API call.
-            this.client.apis.default
-                .get_orders__zipCode_({ zipCode: this.zipCode })
-                .then(response => {
-                    this.assignments = response.body.map(assignment => {
-                        assignment.date = parseISO(assignment.date);
+      // Make API call.
+      this.client.apis.default
+        .get_orders__zipCode_({ zipCode: this.zipCode })
+        .then(response => {
+          this.assignments = response.body.map(assignment => {
+            assignment.date = parseISO(assignment.date);
 
-                        return assignment;
-                    });
-                })
-                .catch(error => {
-                    // @todo Handle error?
-                    console.error(error);
-                })
-                .finally(() => {
-                    // Unset loading state.
-                    this.loading = false;
-                });
-        }
-    },
-
-    async created() {
-        // Fetch API client and store locally.
-        this.client = await api;
+            return assignment;
+          });
+        })
+        .catch(error => {
+          // @todo Handle error?
+          console.error(error);
+        })
+        .finally(() => {
+          // Unset loading state.
+          this.loading = false;
+        });
     }
+  },
+
+  async created() {
+    // Fetch API client and store locally.
+    this.client = await api;
+  }
 };
 </script>
 
@@ -109,28 +109,28 @@ export default {
 @import "@/components/Branding.scss";
 
 .plzfilterinput {
-    .control {
-        width: 90px;
-    }
+  .control {
+    width: 90px;
+  }
 }
 
 .overview-wrapper {
-    border-radius: 4px;
-    overflow: hidden;
-    // box-shadow: 0px 4px 8px rgba($primary, 0.24);
+  border-radius: 4px;
+  overflow: hidden;
+  // box-shadow: 0px 4px 8px rgba($primary, 0.24);
+  width: 100%;
+  max-width: 320px;
+  @media only screen and (min-width: 560px) {
     width: 100%;
-    max-width: 320px;
-    @media only screen and (min-width: 560px) {
-        width: 100%;
-        max-width: 512px;
+    max-width: 512px;
+  }
+  .title {
+    // background-color: $primary;
+    span {
+      font-weight: 700;
+      color: $blue;
     }
-    .title {
-        // background-color: $primary;
-        span {
-            font-weight: 700;
-            color: $blue;
-        }
-        padding: 16px 0 16px 0;
-    }
+    padding: 16px 0 16px 0;
+  }
 }
 </style>
--- frontend/src/components/organisms/KkUserNavigation.vue
+++ frontend/src/components/organisms/KkUserNavigation.vue
@@ -1,77 +1,77 @@
 <template>
-    <nav class="navigation normalize-width">
-        <div class="profile-pic" @click="$router.push('/mein-profil')">
-            <img src="/dummy_profile.jpg" alt />
+  <nav class="navigation normalize-width">
+    <div class="profile-pic" @click="$router.push('/mein-profil')">
+      <img src="/dummy_profile.jpg" alt />
+    </div>
+
+    <div class="right">
+      <div class="karma" @click="$router.push('/leader-board')">
+        <KarmaIcon />
+      </div>
+
+      <div class="hamburger-menu" @click="expandMenu = true">
+        <i class="fas fa-bars"></i>
+      </div>
+
+      <div class="menu-items">
+        <ul>
+          <li>
+            <router-link to="/mein-profil">mein profil</router-link>
+          </li>
+          <li>
+            <router-link to="/meine-auftraege">meine aufräge</router-link>
+          </li>
+          <li>
+            <router-link to="auftraege">missionen finden</router-link>
+          </li>
+          <li @click="logOut()">
+            <router-link to="einloggen">ausloggen</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="open-menu" :class="{expand: expandMenu}">
+      <div class="top normalize-width">
+        <div class="logo" @click="$router.push('/mein-profil')">
+          <img src="@/assets/logo_inverted.svg" alt="Karmakurier Logo" />
         </div>
 
-        <div class="right">
-            <div class="karma" @click="$router.push('/leader-board')">
-                <KarmaIcon />
-            </div>
-
-            <div class="hamburger-menu" @click="expandMenu = true">
-                <i class="fas fa-bars"></i>
-            </div>
-
-            <div class="menu-items">
-                <ul>
-                    <li>
-                        <router-link to="/mein-profil">mein profil</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/meine-auftraege">meine aufräge</router-link>
-                    </li>
-                    <li>
-                        <router-link to="auftraege">aufträge finden</router-link>
-                    </li>
-                    <li @click="logOut()">
-                        <router-link to="einloggen">ausloggen</router-link>
-                    </li>
-                </ul>
-            </div>
+        <div class="hamburger-menu" @click="expandMenu = false">
+          <i class="fas fa-times"></i>
         </div>
-
-        <div class="open-menu" :class="{expand: expandMenu}">
-            <div class="top normalize-width">
-                <div class="logo" @click="$router.push('/mein-profil')">
-                    <img src="@/assets/logo_inverted.svg" alt="Karmakurier Logo" />
-                </div>
-
-                <div class="hamburger-menu" @click="expandMenu = false">
-                    <i class="fas fa-times"></i>
-                </div>
-            </div>
-            <div class="items normalize-width">
-                <ul>
-                    <li @click="$router.push('/mein-profil'); expandMenu = false">Mein Profil</li>
-                    <li @click="$router.push('/meine-auftraege'); expandMenu = false">Meine Aufträge</li>
-                    <li @click="$router.push('/auftraege'); expandMenu = false">Aufträge finden</li>
-                    <li @click="logOut(); expandMenu = false">Ausloggen</li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+      </div>
+      <div class="items normalize-width">
+        <ul>
+          <li @click="$router.push('/mein-profil'); expandMenu = false">Mein Profil</li>
+          <li @click="$router.push('/meine-auftraege'); expandMenu = false">Meine Missionen</li>
+          <li @click="$router.push('/auftraege'); expandMenu = false">Missionen finden</li>
+          <li @click="logOut(); expandMenu = false">Ausloggen</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 import KarmaIcon from "@/components/atoms/KarmaIcon.vue";
 
 export default {
-    data() {
-        return {
-            expandMenu: false
-        };
-    },
-    components: {
-        KarmaIcon
-    },
-    props: {},
-    methods: {
-        logOut() {
-            this.$store.dispatch("logOut");
-            this.$router.push("/einloggen");
-        }
+  data() {
+    return {
+      expandMenu: false
+    };
+  },
+  components: {
+    KarmaIcon
+  },
+  props: {},
+  methods: {
+    logOut() {
+      this.$store.dispatch("logOut");
+      this.$router.push("/einloggen");
     }
+  }
 };
 </script>
 
@@ -79,138 +79,138 @@ export default {
 @import "../Branding.scss";
 
 .navigation {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .karma {
-    cursor: pointer;
-    width: 4rem;
-    @media only screen and (min-width: 910px) {
-        width: 3rem;
-    }
+  cursor: pointer;
+  width: 4rem;
+  @media only screen and (min-width: 910px) {
+    width: 3rem;
+  }
 }
 
 .hamburger-menu {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    border-radius: 100%;
-    height: 40px;
-    width: 40px;
-    &:hover {
-        background-color: rgba($primary, 0.16);
-    }
-    @media only screen and (min-width: 910px) {
-        display: none;
-    }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 100%;
+  height: 40px;
+  width: 40px;
+  &:hover {
+    background-color: rgba($primary, 0.16);
+  }
+  @media only screen and (min-width: 910px) {
+    display: none;
+  }
 }
 
 .fas {
-    display: inline-block;
-    font-size: 22px;
+  display: inline-block;
+  font-size: 22px;
 }
 
 .profile-pic {
-    img {
-        width: 56px;
-        height: 56px;
-        border-radius: 100%;
-        border: solid 2px $blue;
-    }
+  img {
+    width: 56px;
+    height: 56px;
+    border-radius: 100%;
+    border: solid 2px $blue;
+  }
 }
 
 .menu-items {
-    display: none;
-    @media only screen and (min-width: 910px) {
-        display: block;
-    }
-    ul {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        list-style: none;
-        margin: 0;
-        padding: 0;
-        li {
-            font-weight: 700;
-            a {
-                color: $black;
-                padding-left: 40px;
-            }
-            img {
-                margin-left: 16px;
-                width: 48px;
-                height: 48px;
-                border-radius: 100%;
-            }
-            &.helper-login {
-                a {
-                    color: $blue;
-                }
-            }
-            &:hover {
-                cursor: pointer;
-                a {
-                    color: $primary;
-                }
-            }
-        }
-    }
-}
-
-.right {
+  display: none;
+  @media only screen and (min-width: 910px) {
+    display: block;
+  }
+  ul {
     display: flex;
     justify-content: flex-end;
     align-items: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    li {
+      font-weight: 700;
+      a {
+        color: $black;
+        padding-left: 40px;
+      }
+      img {
+        margin-left: 16px;
+        width: 48px;
+        height: 48px;
+        border-radius: 100%;
+      }
+      &.helper-login {
+        a {
+          color: $blue;
+        }
+      }
+      &:hover {
+        cursor: pointer;
+        a {
+          color: $primary;
+        }
+      }
+    }
+  }
+}
+
+.right {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
 }
 
 .open-menu {
-    z-index: 99;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 0%;
-    background-color: $primary;
-    overflow: hidden;
-    transition: all 0.16s ease;
+  z-index: 99;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 0%;
+  background-color: $primary;
+  overflow: hidden;
+  transition: all 0.16s ease;
 
-    &.expand {
-        height: 100%;
+  &.expand {
+    height: 100%;
+  }
+
+  .top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    i {
+      color: $blue;
     }
+  }
 
-    .top {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        i {
-            color: $blue;
+  .hamburger-menu {
+    margin-top: 12px;
+  }
+
+  .items {
+    ul {
+      margin: 0;
+      width: 100%;
+      li {
+        padding: 16px 0;
+        text-align: center;
+        font-weight: 700;
+        font-size: 1.6rem;
+        color: white;
+        cursor: pointer;
+        &:hover {
+          background-color: #ff9065;
         }
+      }
     }
-
-    .hamburger-menu {
-        margin-top: 12px;
-    }
-
-    .items {
-        ul {
-            margin: 0;
-            width: 100%;
-            li {
-                padding: 16px 0;
-                text-align: center;
-                font-weight: 700;
-                font-size: 1.6rem;
-                color: white;
-                cursor: pointer;
-                &:hover {
-                    background-color: #ff9065;
-                }
-            }
-        }
-    }
+  }
 }
 </style>
