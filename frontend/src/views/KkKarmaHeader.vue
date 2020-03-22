ca7ae4c916bc78078011354e12cfb8c4642fec78
Mathias Wellner <mathias@mwellner.de>
2020-03-22 15:18:09 +0100

    remove console.log

--- frontend/src/views/KkKarmaHeader.vue
+++ frontend/src/views/KkKarmaHeader.vue
@@ -1,7 +1,6 @@
 <template>
-    <div>
-
-        <!--
+  <div>
+    <!--
         <div class="pointbg">
             <div
                     class="karma-point"
@@ -14,240 +13,251 @@
                 </b-tooltip>
             </div>
         </div>
-        -->
+    -->
 
-        <canvas ref="cv"></canvas>
+    <canvas ref="cv"></canvas>
 
-        <div>
-            <hr>
-            <hr>
-        </div>
-
-        <div class="badge">
-            <div class="ribbons">
-                <img src="@/assets/illustrations/badge_ribbons.svg" />
-            </div>
-            <div class="circle">
-                <img src="@/assets/illustrations/badge.svg" />
-            </div>
-            <div class="points">{{points}}</div>
-            <div class="plabel">karmapunkte</div>
-        </div>
-
-
-
+    <div>
+      <hr />
+      <hr />
     </div>
+
+    <div class="badge">
+      <div class="ribbons">
+        <img src="@/assets/illustrations/badge_ribbons.svg" />
+      </div>
+      <div class="circle">
+        <img src="@/assets/illustrations/badge.svg" />
+      </div>
+      <div class="points">{{points}}</div>
+      <div class="plabel">karmapunkte</div>
+    </div>
+  </div>
 </template>
 
 <script>
+var SCREEN_WIDTH = window.innerWidth;
+var SCREEN_HEIGHT = window.innerHeight;
 
-    var SCREEN_WIDTH = window.innerWidth;
-    var SCREEN_HEIGHT = window.innerHeight;
+var RADIUS = 300;
 
-    var RADIUS = 300;
+var RADIUS_SCALE = 1;
+var RADIUS_SCALE_MIN = 1;
+var RADIUS_SCALE_MAX = 1.5;
 
-    var RADIUS_SCALE = 1;
-    var RADIUS_SCALE_MIN = 1;
-    var RADIUS_SCALE_MAX = 1.5;
+var mouseIsDown = false;
 
-    var mouseIsDown = false;
+export default {
+  name: "KkKarmaHeader",
+  data() {
+    return {
+      points: 36,
+      ctx: null,
+      particles: []
+    };
+  },
+  methods: {
+    randomNumber(index) {
+      let randomNumbers = [];
+      for (let i = 0; i < 34; i += 1) {
+        randomNumbers.push(Math.round(Math.random() * 100));
+      }
+      return randomNumbers[index];
+    },
 
+    createParticles() {
+      this.particles = [];
 
+      for (var i = 0; i < this.points; i++) {
+        var particle = {
+          size: 1,
+          position: {
+            x: (Math.random() >= 0.5 ? 0 : 1) * window.innerWidth,
+            y: Math.random() * 340
+          },
+          offset: { x: 0, y: 0 },
+          shift: {
+            x: (Math.random() >= 0.5 ? 0 : 1) * window.innerWidth,
+            y: Math.random() * 340
+          },
+          speed: 0.002 + Math.random() * 0.008,
+          targetSize: 1,
+          fillColor: "hsl(17, 100%, 72%)",
+          orbit: RADIUS * 0.5 + RADIUS * 0.5 * Math.random()
+        };
 
-    export default {
-        name: "KkKarmaHeader",
-        data() {
-            return {
-                points: 36,
-                ctx: null,
-                particles: []
-            }
-        },
-        methods: {
-            randomNumber(index) {
-                let randomNumbers = [];
-                for (let i = 0; i < 34; i += 1) {
-                    randomNumbers.push(Math.round(Math.random() * 100));
-                }
-                return randomNumbers[index];
-            },
+        this.particles.push(particle);
+      }
+    },
 
-            createParticles() {
+    loop() {
+      if (mouseIsDown) {
+        RADIUS_SCALE += (RADIUS_SCALE_MAX - RADIUS_SCALE) * 0.02;
+      } else {
+        RADIUS_SCALE -= (RADIUS_SCALE - RADIUS_SCALE_MIN) * 0.02;
+      }
 
-                this.particles = [];
+      RADIUS_SCALE = Math.min(RADIUS_SCALE, RADIUS_SCALE_MAX);
 
-                for (var i = 0; i < this.points; i++) {
-                    var particle = {
-                        size: 1,
-                        position: { x: (Math.random() >= 0.5 ? 0 : 1) * window.innerWidth, y: Math.random() * 340 },
-                        offset: { x: 0, y: 0 },
-                        shift: { x: (Math.random() >= 0.5 ? 0 : 1) * window.innerWidth, y: Math.random() * 340 },
-                        speed: 0.002+Math.random()*0.008,
-                        targetSize: 1,
-                        fillColor: 'hsl(17, 100%, 72%)',
-                        orbit: RADIUS*.5 + (RADIUS * .5 * Math.random())
-                    };
+      this.ctx.fillStyle = "rgba(255,255,255,0.7)";
+      this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
 
-                    console.log(particle.position);
+      var mouseX = window.innerWidth / 2; //dirty
+      var mouseY = 150;
 
-                    this.particles.push( particle );
-                }
-            },
+      for (var i = 0, len = this.particles.length; i < len; i++) {
+        var particle = this.particles[i];
 
+        var lp = { x: particle.position.x, y: particle.position.y };
 
-            loop() {
+        // Rotation
+        particle.offset.x += particle.speed;
+        particle.offset.y += particle.speed;
 
-                if (mouseIsDown) {
-                    RADIUS_SCALE += (RADIUS_SCALE_MAX - RADIUS_SCALE) * (0.02);
-                } else {
-                    RADIUS_SCALE -= (RADIUS_SCALE - RADIUS_SCALE_MIN) * (0.02);
-                }
+        // Follow mouse with some lag
+        particle.shift.x += (mouseX - particle.shift.x) * particle.speed;
+        particle.shift.y += (mouseY - particle.shift.y) * particle.speed;
 
-                RADIUS_SCALE = Math.min(RADIUS_SCALE, RADIUS_SCALE_MAX);
+        // Apply position
+        particle.position.x =
+          particle.shift.x +
+          Math.cos(i + particle.offset.x) * (particle.orbit * RADIUS_SCALE);
+        particle.position.y =
+          particle.shift.y +
+          Math.sin(i + particle.offset.y) * (particle.orbit * RADIUS_SCALE);
 
-                this.ctx.fillStyle = 'rgba(255,255,255,0.7)';
-                this.ctx.fillRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+        // Limit to screen bounds
+        particle.position.x = Math.max(
+          Math.min(particle.position.x, SCREEN_WIDTH),
+          0
+        );
+        particle.position.y = Math.max(
+          Math.min(particle.position.y, SCREEN_HEIGHT),
+          0
+        );
 
-                var mouseX = window.innerWidth/2; //dirty
-                var mouseY = 150;
+        particle.size += (particle.targetSize - particle.size) * 0.05;
 
-                for (var i = 0, len = this.particles.length; i < len; i++) {
-                    var particle = this.particles[i];
-
-                    var lp = {x: particle.position.x, y: particle.position.y};
-
-                    // Rotation
-                    particle.offset.x += particle.speed;
-                    particle.offset.y += particle.speed;
-
-                    // Follow mouse with some lag
-                    particle.shift.x += (mouseX - particle.shift.x) * (particle.speed);
-                    particle.shift.y += (mouseY - particle.shift.y) * (particle.speed);
-
-                    // Apply position
-                    particle.position.x = particle.shift.x + Math.cos(i + particle.offset.x) * (particle.orbit * RADIUS_SCALE);
-                    particle.position.y = particle.shift.y + Math.sin(i + particle.offset.y) * (particle.orbit * RADIUS_SCALE);
-
-                    // Limit to screen bounds
-                    particle.position.x = Math.max(Math.min(particle.position.x, SCREEN_WIDTH), 0);
-                    particle.position.y = Math.max(Math.min(particle.position.y, SCREEN_HEIGHT), 0);
-
-                    particle.size += (particle.targetSize - particle.size) * 0.05;
-
-                    if (Math.round(particle.size) == Math.round(particle.targetSize)) {
-                        particle.targetSize = 1 + Math.random() * 7;
-                    }
-
-                    this.ctx.beginPath();
-                    this.ctx.fillStyle = particle.fillColor;
-                    this.ctx.strokeStyle = particle.fillColor;
-                    this.ctx.lineWidth = particle.size;
-                    this.ctx.moveTo(lp.x, lp.y);
-                    this.ctx.lineTo(particle.position.x, particle.position.y);
-                    this.ctx.stroke();
-                    this.ctx.arc(particle.position.x, particle.position.y, particle.size / 2, 0, Math.PI * 2, true);
-                    this.ctx.fill();
-                }
-            }
-        },
-        mounted() {
-
-            window.addEventListener('resize', ()=>{
-                this.$refs.cv.width = window.innerWidth;
-                this.$refs.cv.height = 340;
-            }, false);
-
-            this.$refs.cv.width = window.innerWidth;
-            this.$refs.cv.height = 340;
-
-
-            this.ctx = this.$refs.cv.getContext('2d');
-
-            //this is dirty, solle windowanimationframe sein
-            this.createParticles();
-            setInterval( this.loop, 1000 / 60 );
+        if (Math.round(particle.size) == Math.round(particle.targetSize)) {
+          particle.targetSize = 1 + Math.random() * 7;
         }
+
+        this.ctx.beginPath();
+        this.ctx.fillStyle = particle.fillColor;
+        this.ctx.strokeStyle = particle.fillColor;
+        this.ctx.lineWidth = particle.size;
+        this.ctx.moveTo(lp.x, lp.y);
+        this.ctx.lineTo(particle.position.x, particle.position.y);
+        this.ctx.stroke();
+        this.ctx.arc(
+          particle.position.x,
+          particle.position.y,
+          particle.size / 2,
+          0,
+          Math.PI * 2,
+          true
+        );
+        this.ctx.fill();
+      }
     }
+  },
+  mounted() {
+    window.addEventListener(
+      "resize",
+      () => {
+        this.$refs.cv.width = window.innerWidth;
+        this.$refs.cv.height = 340;
+      },
+      false
+    );
+
+    this.$refs.cv.width = window.innerWidth;
+    this.$refs.cv.height = 340;
+
+    this.ctx = this.$refs.cv.getContext("2d");
+
+    //this is dirty, solle windowanimationframe sein
+    this.createParticles();
+    setInterval(this.loop, 1000 / 60);
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    @import "@/components/Branding.scss";
+@import "@/components/Branding.scss";
 
-    .badge {
-        width: 300px;
-        height: 340px;
-        position: relative;
-        margin: auto;
+.badge {
+  width: 300px;
+  height: 340px;
+  position: relative;
+  margin: auto;
 
-        > div {
-            width: 300px;
-        }
+  > div {
+    width: 300px;
+  }
 
-        .circle {
-            position: absolute;
-            top: 0;
-            left: 0;
+  .circle {
+    position: absolute;
+    top: 0;
+    left: 0;
 
-            animation-name: ckw;
-            animation-duration: 60s;
-        }
+    animation-name: ckw;
+    animation-duration: 60s;
+  }
 
-        .ribbons {
-            position: absolute;
-            top: 25px;
-            left: 0;
-        }
+  .ribbons {
+    position: absolute;
+    top: 25px;
+    left: 0;
+  }
 
-        .points {
-            position: absolute;
-            top: 104px;
-            left: 0;
+  .points {
+    position: absolute;
+    top: 104px;
+    left: 0;
 
-            font-family: "Quicksand", sans-serif;
-            font-weight: bold;
-            text-align: center;
-            font-size: 50px;
+    font-family: "Quicksand", sans-serif;
+    font-weight: bold;
+    text-align: center;
+    font-size: 50px;
+  }
 
-        }
+  .plabel {
+    position: absolute;
+    top: 162px;
+    left: 0;
 
-        .plabel {
-            position: absolute;
-            top: 162px;
-            left: 0;
+    text-align: center;
+    color: $secondary;
+  }
+}
 
-            text-align: center;
-            color: $secondary;
-        }
+@keyframes ckw {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 
-    }
+hr {
+  border-bottom: 3px solid $primary;
 
-    @keyframes ckw {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
-    }
+  position: absolute;
 
+  top: 150px;
+  left: 20px;
 
-    hr {
-        border-bottom: 3px solid $primary;
+  width: 35%;
 
-        position: absolute;
+  &:last-child {
+    right: 20px;
+    left: initial;
+  }
+}
 
-        top: 150px;
-        left: 20px;
-
-        width: 35%;
-
-        &:last-child {
-            right: 20px;
-            left: initial;
-        }
-    }
-
-    /*
+/*
     .karma-point {
         position: absolute;
         height: 10px;
@@ -274,8 +284,7 @@
         }
     }
 */
-    canvas {
-        position: absolute;
-    }
-
+canvas {
+  position: absolute;
+}
 </style>
