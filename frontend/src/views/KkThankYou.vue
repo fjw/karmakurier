0b44c24307a48a3452d18e8fd7c5ff6e085c97f2
fjw <fjw@garx.de>
2020-03-23 23:05:33 +0100

    Texte / Abstände Schmutzige furchtbare brs raus

--- frontend/src/views/KkThankYou.vue
+++ frontend/src/views/KkThankYou.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="thank-you-wrapper">
         <div class="title">Vielen Dank!</div>
-        <br>
+
         <div class="subtitle">Wir werden schnellstmöglich einen Helfer finden, der Kontakt zu Ihnen aufnimmt.</div>
         <div class="subtitle">Es wird sich in Kürze jemand bei Ihnen melden - wir bitten um etwas Geduld.</div>
-        <br>
+
         <div class="subtitle">Kontaktlose Lieferung</div>
         <div class="text">Gerade jetzt ist es besonders wichtig, bewusst und verantwortungsvoll im zwischenmenschlichen Kontakt zu sein. Daher gilt es das Übertragungsrisiko zu minimieren und eine kontaktlose Übergabe zwischen unseren Helfern und Hilfesuchenden sicherzustellen.</div>
-        <br>
+
         <div class="text">Für die Übergabe der Besorgungen bedeutet dies, dass der Helfende die Lieferung vor der Wohnungstür abstellt und sich zwei Meter von ihr entfernt. Daraufhin wird die Lieferung von Ihnen entgegengenommen und der Helfende geht wieder.</div>
-        <br>
+
         <div class="video">
             <iframe
                 width="560"
@@ -20,12 +20,12 @@
                 allowfullscreen
             ></iframe>
         </div>
-        <br>
+
         <div class="subtitle">Bezahlung</div>
         <div class="text">Bestenfalls bedeutet dies auch, dass Sie online bezahlen. Falls Sie Bar bezahlen möchten, legen Sie bitte das Geld auf die Fussmatte und entfernen sich ebenfalls, damit der Helfende das Geld aufnehmen kann.</div>
-        <br>
+
         <div class="text">Die Ansteckungswege sind noch nicht sicher geklärt und die Ansteckung über Flächen nicht ausgeschlossen. Wir empfehlen in jedem Fall die Hände zu waschen bevor und nachdem man mit der Ware oder dem desinfizierten Bargeld in Berührung kommt.</div>
-        <br>
+
         <div class="video">
             <iframe
                 width="560"
@@ -36,8 +36,8 @@
                 allowfullscreen
             ></iframe>
         </div>
-        <br>
-        <div class="text">Nun ist die Mission abgeschlossen - bis zum nächsten Mal!</div>
+
+        <div class="title">Nun ist die Mission abgeschlossen - bis zum nächsten Mal!</div>
     </div>
 </template>
 
@@ -48,15 +48,27 @@ export default {};
 <style lang="scss" scoped>
 @import "@/components/Branding.scss";
 
+.subtitle {
+    margin-top: 30px;
+}
+
+.text {
+    margin: 10px 0;
+}
+
 .thank-you-wrapper {
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     padding: 64px 16px 64px 16px;
+    max-width: 512px;
+    margin: auto;
+    text-align: center;
 }
 
 .video {
+    margin: 30px 0;
     height: 315px;
     iframe {
         height: 100%;
