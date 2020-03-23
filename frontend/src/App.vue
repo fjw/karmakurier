32ce0b4615e051b4364593c3a8e2d45a69b820be
fjw <fjw@garx.de>
2020-03-23 22:07:54 +0100

    mach es gelb und fixier es ganz dolle

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,15 +6,17 @@
         </template>
         <router-view />
         <KkFooter />
-        <div class="normalize-width">
-            <div class="demo">
-                <img src="@/assets/karmakurier_achtung.svg" alt="achtung icon" />
-                <div class="text">
-                    Achtung Prototyp. Beliebige Zugangsdaten können verwendet werden. Bitte liked und teilt
-                    <a
-                        href="https://www.youtube.com/watch?v=6Kr9JdEWCt8"
-                        target="_blank"
-                    >unser Video auf Youtube.</a>
+        <div class="demo-wrapper">
+            <div class="normalize-width">
+                <div class="demo">
+                    <img src="@/assets/karmakurier_achtung.svg" alt="achtung icon" />
+                    <div class="text">
+                        Achtung Prototyp. Beliebige Zugangsdaten können verwendet werden. Bitte liked und teilt
+                        <a
+                            href="https://www.youtube.com/watch?v=6Kr9JdEWCt8"
+                            target="_blank"
+                        >unser Video auf Youtube.</a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -51,6 +53,7 @@ body {
 #app {
     margin-top: 8px;
     min-height: 100%;
+    margin-bottom: 60px;
 }
 
 .normalize-width {
@@ -63,43 +66,58 @@ body {
     }
 }
 
-.demo {
-    border: solid 3px $primary;
-    border-radius: 8px;
-    padding: 8px 12px 8px 12px;
-    margin: 8px 0;
-    font-weight: 700;
-    text-align: left;
-    display: inline-flex;
-    justify-content: space-between;
-    align-items: center;
-    .text {
-        display: inline;
-    }
-    a {
-        margin-left: 4px;
-        &:hover {
-            text-decoration: underline;
-            color: $primary;
+.demo-wrapper {
+
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+
+    .demo {
+        border: solid 3px gold;
+        border-radius: 8px;
+        padding: 8px 12px 8px 12px;
+        margin: 8px 0;
+        font-weight: 700;
+        text-align: left;
+        display: inline-flex;
+        justify-content: space-between;
+        align-items: center;
+        background: white;
+        text-align: center;
+
+        .text {
+            display: inline;
         }
-    }
-    img {
-        display: inline-block;
-        height: 32px;
-        width: 32px;
-        margin-right: 16px;
-    }
-    @media only screen and (min-width: 512px) {
-        display: block;
+
+        a {
+            margin-left: 4px;
+
+            &:hover {
+                text-decoration: underline;
+                color: $primary;
+            }
+        }
+
         img {
-            margin-bottom: -6px;
+            display: inline-block;
+            height: 32px;
+            width: 32px;
+            margin-right: 16px;
         }
-    }
-    @media only screen and (min-width: 1100px) {
-        img {
-            margin-bottom: -10px;
-            margin-right: 8px;
+
+        @media only screen and (min-width: 512px) {
+            display: block;
+            img {
+                margin-bottom: -6px;
+            }
         }
+        @media only screen and (min-width: 1100px) {
+            img {
+                margin-bottom: -10px;
+                margin-right: 8px;
+            }
+        }
+
     }
 }
 </style>
